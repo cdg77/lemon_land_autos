6e16fdabd7833d269b297729b9e2dde40058f5dd
Rachael Berecka & Carlo DeGregorio <student@epicodus.com>
2015-08-12 10:28:00 -0700

    it checks if the auto is worth buying(2 tests passing)

--- lib/lemon.rb
+++ lib/lemon.rb
@@ -1,4 +1,4 @@
-class Vehicle 
+class Vehicle
   define_method(:initialize) do |make, model, year|
     @year = year
     @model = model
@@ -9,4 +9,12 @@ class Vehicle
     current_year = Time.new().year()
     current_year.-(@year)
   end
+
+  define_method(:worth_buying?) do
+    american_cars = ['Chrysler','Cheverolet', 'Ford', 'GM', 'Dodge', 'Oldsmobile']
+    american = american_cars.include?(@make)
+    new_enough = self.age().<=(15)
+    !(american.&(new_enough))
+  end
+
 end
--- spec/lemon_spec.rb
+++ spec/lemon_spec.rb
@@ -8,4 +8,12 @@ describe(Vehicle) do
       expect(test_vehicle.age()).to(eq(15))
     end
   end
+
+  describe('#worth_buying?') do
+    it('returns true if the vehicle is not american made and older than 15 years old') do
+      test_vehicle = Vehicle.new('Toyota', 'Prius', 2000)
+      expect(test_vehicle.worth_buying?()).to(eq(true))
+    end
+  end
+
 end
