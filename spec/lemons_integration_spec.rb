ef18d9547586776eeb21068fc1d2dfad124df09c
Rachael Berecka & Carlo DeGregorio <student@epicodus.com>
2015-08-12 15:49:10 -0700

    It checks the file path for index page

--- spec/lemons_integration_spec.rb
+++ spec/lemons_integration_spec.rb
@@ -2,3 +2,10 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+describe('the path of lemon', {:type => :feature}) do
+  it('shows the index page')  do
+    visit('/')
+    expect(page).to have_content("please don't")
+  end
+end
