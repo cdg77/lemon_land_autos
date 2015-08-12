7a3f861b0d376eba42419f53266d290d5183318b
Rachael Berecka & Carlo DeGregorio <student@epicodus.com>
2015-08-12 11:30:47 -0700

    Add year method spec.

--- spec/lemon_spec.rb
+++ spec/lemon_spec.rb
@@ -35,6 +35,14 @@ describe(Vehicle) do
     end
   end
 
+  describe("#year") do
+    it("returns the year of the vehicle") do
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      test_vehicle.save()
+      expect(test_vehicle.year()).to(eq(2000))
+    end
+  end
+
   describe("#save") do
     it("adds a vehicle to the array of saved vehicles") do
       test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
